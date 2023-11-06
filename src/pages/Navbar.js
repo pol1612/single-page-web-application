ae74d2a00d3b3e2f23dbdf9379ae803f09c7045e
Pol Sañé Jové <pol.sane@uvic.cat>
2023-11-06 11:27:42 +0000

    initial commit

---
+++ src/pages/Navbar.js
@@ -0,0 +1,15 @@
+// Navbar.js
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <nav>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/products">Products</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
+  </nav>
+  );
+}
+
